da297442d6ab3177a211e160aad273188b82feff
Gladkovian <gladkovian@gmail.com>
2022-06-17 09:38:19 +0300

    bash

---
+++ tag.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -x
+
+tag=$(curl -s  https://github.com/gladkovia/ci-cd-java/pkgs/container/ci-cd-java%2Fci-cd-java | grep latest | awk 'FS="tag/s"{print$9}' | head -1 | sed 's/"//' | cut -d ':' -f2)
+val=$(cat /var/lib/jenkins/workspace/ci-cd-github-jenkins-k8s/java-deploy-helm/values.yaml | awk 'FS="image: "{print$2}' | tr -s '\n' | tail -1 | cut -d ':' -f2)
+file=$(/var/lib/jenkins/workspace/ci-cd-github-jenkins-k8s/java-deploy-helm/values.yaml)
+
+if [[ $tag == $val ]]; then
+  echo "yes"
+elif [[ $tag != $val ]]; then 
+#  echo "No"
+  sed -i 's/$val/$tag/' /var/lib/jenkins/workspace/ci-cd-github-jenkins-k8s/java-deploy-helm/values.yaml
+fi
